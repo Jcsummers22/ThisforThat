5b69e6cb3a942ae64bef05c9dd9920409ff099f0
Laura Unaeze <l.unaeze5@gmail.com>
2017-11-02 22:18:19 -0700

    Fixed syntax for models. Put each model in its own file. Deleted thisforthat.js

---
+++ models/itemModel.js
@@ -0,0 +1,42 @@
+// creating Buyer model
+// Exporting makes the Buyer Model available for other files.
+// This will also create a table when server.js is run.
+module.exports = function(sequelize, DataTypes) {
+  var Item = sequelize.define("Item", {
+    item_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    owner_id: {
+      type: DataTypes.INTEGER
+    },
+    catergory: {
+      type: DataTypes.STRING
+    },
+    // two options: sale or trade
+    // default true = sale
+    // false = service
+    good_or_service: {
+      type: DataTypes.BOOLEAN
+    },
+    // allow the user to provide good/service for money
+    for_cash: {
+      type: DataTypes.BOOLEAN
+    },
+    // allow the user to provide good/service for trade of good
+    trade_good: {
+      type: DataTypes.BOOLEAN
+    },
+    // allow the user to provide good/service for trade of service
+    trade_service: {
+      type: DataTypes.BOOLEAN
+    },
+    buyer_id: {
+      type: DataTypes.INTEGER
+    }
+  }, {
+    timestamps: true
+  });
+
+  return Item;
+}
