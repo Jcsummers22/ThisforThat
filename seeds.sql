a91bf1cedfb03caffcba8891df225318d029504a
Jason Summers <jcsummers04@gmail.com>
2017-11-04 12:07:18 -0700

    Updated modals, css, frameworks

---
+++ seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO thisforthat.users(userName, firstName, lastName, email, location)
+VALUES ("launaeze", "Laura", "Unaeze", "email@email.com", "Phoenix, AZ");
+
+INSERT INTO thisforthat.items(item_name, category, owner_id)
+VALUES ("boots", "clothes", 1);
+
+
+SELECT * FROM thisforthat.items;
+SELECT * FROM thisforthat.users;
--- server.js
+++ server.js
@@ -1,32 +1,42 @@
+// Bring in dependencies
 var express = require('express');  
 var methodOverride = require('method-override'); 
 var bodyParser = require('body-parser');
-var app = express(); 
- 
-
 var exphbs = require('express-handlebars');
 
+// Set up Express App
+var app = express(); 
+var PORT = process.env.PORT || 3000;
+ 
 
 //static content
-app.use(express.static(process.cwd() + '/public'));
+app.use(express.static('./public'));
 
+// require everything in the models folder
+var db = require("./models/");
 
-app.use(bodyParser.urlencoded({
-	extended: false
-})); 
-
+// Setting up Express to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 //override with POST command
 app.use(methodOverride('_method')); 
- 
+
+// Sets Handlebars as the default templating engine.
 app.engine('handlebars', exphbs({
 	defaultLayout: 'main'
 })); 
 
 app.set('view engine', 'handlebars'); 
 
-var routes = require('./controllers/swap_controller.js'); 
-app.use('/', routes); 
+// The routes
+require("./routes/item-api-routes.js")(app);
 
- 
-app.listen(process.env.PORT || 3000); 
+// Starts our Express server
+db.sequelize.sync({ force: true }).then(function() {
+	app.listen(PORT, function() {
+  	console.log("App listening on PORT " + PORT);
+	});
+});
