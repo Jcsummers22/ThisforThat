0eba9336cc6f6886a463e0fe0ff4ecb9bfda6f7f
Laura Unaeze <l.unaeze5@gmail.com>
2017-11-04 11:15:23 -0700

    Fixed javascript for the modal pop-up.

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   
     $(document).ready(function(){
       // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-      $('.modal-trigger').leanModal();
+      $('.modal').modal();
         dismissible: true;
         closeIcon: true
         $('.close').closeModal();
